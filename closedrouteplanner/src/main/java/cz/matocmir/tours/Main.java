9d4089e2cd0d07244fa80f8d548fab9d906b10ff
miroslavmatocha <matocham254@gmail.com>
2019-02-17 08:59:02 +0100

    Solution proposal section added Main class refactored for easier testing

--- closedrouteplanner/src/main/java/cz/matocmir/tours/Main.java
+++ closedrouteplanner/src/main/java/cz/matocmir/tours/Main.java
@@ -5,55 +5,85 @@ import cz.matocmir.tours.forwardpath.CandidateFinder;
 import cz.matocmir.tours.forwardpath.CandidatesPicker;
 import cz.matocmir.tours.model.*;
 import cz.matocmir.tours.utils.IOUtils;
-import org.apache.log4j.Logger;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
-	private static final Logger log = Logger.getLogger(Main.class);
 
 	public static void main(String[] args) {
 		final String graphPath = "./src/main/resources/prague.csv";
-		log.info("Loading graph from file: " + graphPath);
+		System.out.println("Loading graph from file: " + graphPath);
 		TourGraph loadedGraph = IOUtils.loadGraph(graphPath);
 
 		//System.out.println("Graph loaded ... dumping");
 		//loadedGraph.dumpToGeoJson("test.geojson");
 
-		CandidateFinder canFinder = new CandidateFinder(loadedGraph);
+		while(true){
+			solve(loadedGraph);
+		}
 
-		TourNode startingNode = loadedGraph.getNode(17500);
-		final int minLength = 8500;
-		final int maxLength = 10000;
+	}
 
-		log.info("Running forward search in interval " + minLength + " - " + maxLength + " m");
+	private static void solve(TourGraph graph){
+		Scanner scan = new Scanner(System.in);
+		CandidateFinder canFinder = new CandidateFinder(graph);
+
+		int desLength;
+		int percentTolerance;
+		System.out.print("Enter desired length (in  m): \n>");
+		desLength = scan.nextInt();
+		System.out.print("Enter maximum deviation (in  %): \n>");
+		percentTolerance = scan.nextInt();
+
+		int minLength = desLength - (desLength * percentTolerance / 100);
+		int maxLength = desLength + (desLength * percentTolerance / 100);
+
+		int startingId;
+		System.out.print("Enter starting node id: \n>");
+		startingId = scan.nextInt();
+		TourNode startingNode = graph.getNode(startingId);
+
+		System.out.println("Running forward search in interval " + minLength + " - " + maxLength + " m");
 		List<Candidate> cands = canFinder.forwardSearch(startingNode, minLength, maxLength);
 		System.out.println("Found: " + cands.size() + " candidates");
 		IOUtils.visualizeNodes(cands.stream().map(c -> c.correspNode.getNode()).collect(Collectors.toList()),
 				"found.geojson");
 
-		int numOfTries = 4;
-		BackPathFinder backPathFinder = new BackPathFinder(loadedGraph);
+		int numOfCycles;
+		System.out.print("Enter num of cycles: \n>");
+		numOfCycles = scan.nextInt();
 
+		BackPathFinder backPathFinder = new BackPathFinder(graph);
 		CandidatesPicker candidatesPicker = new CandidatesPicker(cands, startingNode, minLength);
-		log.info("Choosing " + numOfTries + " candidates");
-		List<Candidate> picked = candidatesPicker.selectCandidates(numOfTries);
-		picked.forEach(c -> log.info(c.correspNode.getNode().getId()));
 
+		int foundCycles = 0;
 		List<TourEdge> foundPath;
-		for (int i = 0; i < numOfTries; i++) {
-			TreeNode randomCand = picked.get(i).correspNode;
-			IOUtils.visualizePath(randomCand.pathFromRoot(), "forw_path_" + i + ".geojson");
+		for(int i = 0;i<cands.size();i++) {
+			Candidate randomCand = candidatesPicker.selectCandidate();
+			IOUtils.visualizePath(randomCand.correspNode.pathFromRoot(), "forw_path_" + i + ".geojson");
 
-			log.info("Finding back path from node " + randomCand.getNode().getId());
-			foundPath = backPathFinder.getPathBack(picked.get(i), startingNode, maxLength, minLength);
+			System.out.println("Finding back path from node " + randomCand.correspNode.getNode().getId());
+			foundPath = backPathFinder.getPathBack(randomCand, startingNode, maxLength, minLength);
 
 			if (foundPath != null) {
 				IOUtils.visualizeEdges(foundPath, "bestWalk_" + i + ".geojson");
+				foundCycles++;
 			} else {
-				log.info("Walk not found");
+				System.out.println("Walk not found");
+			}
+
+			if(foundCycles >= numOfCycles){
+				System.out.println("All cycles found");
+				break;
 			}
 		}
+
+		if(foundCycles < numOfCycles){
+			System.out.println("Not enough cycles found");
+		}
+
+		System.out.println("=================================");
 	}
 }
